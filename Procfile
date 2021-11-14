c45bebb55b95684eec3e7a76b91d4cc805a96f9b
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-11-14 20:17:32 +0300

    add vaccine details

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn fcards.wsgi --log-file -
+web: gunicorn clinica.wsgi --log-file -
--- clinica/wsgi.py
+++ clinica/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fcards project.
+WSGI config for clinica project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fcards.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinica.settings')
 
 application = get_wsgi_application()
