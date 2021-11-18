2afe2aaf5667463f30a3371eebad97b149ebb617
Isaac M <Isaackingori68@gmail.com>
2021-11-18 13:59:55 +0300

    make changes to settings

--- clinica/settings.py
+++ clinica/settings.py
@@ -88,9 +88,6 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = [
-    'the deployed url',
-]
 
 ROOT_URLCONF = 'clinica.urls'
 
